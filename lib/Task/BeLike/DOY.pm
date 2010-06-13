9a902bbffb3382b2df5e8139566b7f8f2269f5c8
Jesse Luehrs <doy@tozt.net>
2010-06-13 15:48:12 -0500

    update dzil stuff

--- lib/Task/BeLike/DOY.pm
+++ lib/Task/BeLike/DOY.pm
@@ -81,52 +81,7 @@ package Task::BeLike::DOY;
 
 =pkg Net::GitHub 0.06 includes ::V2
 
-=head1 BUGS
-
-No known bugs.
-
-Please report any bugs through RT: email
-C<bug-task-belike-doy at rt.cpan.org>, or browse to
-L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Task-BeLike-DOY>.
-
-=head1 SUPPORT
-
-You can find this documentation for this module with the perldoc command.
-
-    perldoc Task::BeLike::DOY
-
-You can also look for information at:
-
-=over 4
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Task-BeLike-DOY>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Task-BeLike-DOY>
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Task-BeLike-DOY>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Task-BeLike-DOY>
-
-=back
-
-=head1 AUTHOR
-
-  Jesse Luehrs <doy at tozt dot net>
-
-=head1 COPYRIGHT AND LICENSE
-
-This software is copyright (c) 2010 by Jesse Luehrs.
-
-This is free software; you can redistribute it and/or modify it under
-the same terms as perl itself.
+=for taskweaver_parsing_is_broken
 
 =cut
 
--- t/000-load.t
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env perl
-use strict;
-use warnings;
-use Test::More tests => 1;
-
-package Foo;
-::use_ok('Task::BeLike::DOY')
-    or ::BAIL_OUT("couldn't load Task::BeLike::DOY");
