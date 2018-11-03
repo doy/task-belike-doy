a5bc5cc3244cfcf4a1833f0f0aef86e5e815284f
Jesse Luehrs <doy@tozt.net>
2018-11-03 18:16:48 -0400

    cut back on a lot of things to install

--- lib/Task/BeLike/DOY.pm
+++ lib/Task/BeLike/DOY.pm
@@ -1,22 +1,6 @@
 package Task::BeLike::DOY;
 #ABSTRACT: modules I use
 
-=pkgroup Useful apps
-
-=pkg App::Nopaste 0.20 has Codepeek pastebin
-
-=pkg App::ph
-
-=pkg App::Termcast 0.06
-
-=pkg App::Ttyrec
-
-=pkg LastFM::Export
-
-=pkg Term::TtyRec::Plus 0.09 for ttyplay and ttytime
-
-=pkg Unicode::Tussle
-
 =pkgroup Development tools
 
 =pkg App::ForkProve
@@ -25,13 +9,9 @@ package Task::BeLike::DOY;
 
 =pkg Carp::Reply
 
-=pkg circular::require
-
 =pkg Data::Dump
 
 =pkg Devel::Cover
-
-=pkg Devel::FindRef
 
 =pkg Devel::NYTProf 3.00
 
@@ -49,107 +29,23 @@ package Task::BeLike::DOY;
 
 =pkg Reply
 
-=pkg Test::Pretty
-
-=pkg Test::Without::Module
-
 =pkgroup Useful optional deps for things
 
-=pkg autodie
-
-=pkg Archive::Zip
-
 =pkg B::Keywords
-
-=pkg Devel::PartialDump
-
-=pkg HTTP::Parser::XS
-
-=pkg JSON
-
-=pkg LWP
-
-=pkg LWP::Protocol::https
-
-=pkg Pod::Coverage::TrustPod
 
 =pkg Proc::InvokeEditor
 
 =pkg Term::ReadLine::Gnu
 
-=pkg Test::Pod
-
-=pkg Test::Pod::Coverage
-
-=pkg YAML
-
 =pkgroup CPAN interaction
 
 =pkg App::cpanminus 1.5007 for build.log prompting
-
-=pkg App::cpanoutdated 0.05 for offline mode
 
 =pkg App::pmuninstall
 
 =pkg App::PAUSE::Comaint
 
-=pkg CPAN::Mini
-
-=pkg Module::Which
-
 =pkg Pod::Cpandoc
-
-=pkgroup Modules I use a lot
-
-=pkg Bread::Board::Declare
-
-=pkg DateTime
-
-=pkg Moose 2.0400
-
-=pkg namespace::autoclean
-
-=pkg Path::Class 0.25 for ->traverse
-
-=pkg Plack
-
-=pkg Scope::Guard
-
-=pkg Term::ProgressBar
-
-=pkg Test::Fatal
-
-=pkg Test::More 0.88
-
-=pkg Test::Requires
-
-=pkgroup Dependencies for my non-CPAN apps
-
-=pkg Clipboard
-
-=pkg Config::INI::Reader
-
-=pkg CPAN::Changes
-
-=pkg CPAN::DistnameInfo
-
-=pkg Data::ICal
-
-=pkg Data::ICal::DateTime
-
-=pkg File::pushd
-
-=pkg IO::Pty::Easy 0.08
-
-=pkg JSON
-
-=pkg Parse::CPAN::Packages::Fast
-
-=pkg Term::ReadKey
-
-=pkg WWW::Mechanize
-
-=pkg WWW::Wunderground::API
 
 =for taskweaver_parsing_is_broken
 
